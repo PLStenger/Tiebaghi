c95779557b59949469cb87b83695370afd443c44
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2022-01-05 14:57:17 +1100

    Update 000_run_all_pipeline_in_one_script.sh

--- 00_scripts/000_run_all_pipeline_in_one_script.sh
+++ 00_scripts/000_run_all_pipeline_in_one_script.sh
@@ -2,8 +2,8 @@
 
 # Run all scripts :
 
-time nohup bash 00_quality_check_by_FastQC.sh &> 00_quality_check_by_FastQC.out
-time nohup bash 01_pooling_sequences.sh &> 01_pooling_sequences.out
+#time nohup bash 00_quality_check_by_FastQC.sh &> 00_quality_check_by_FastQC.out
+#time nohup bash 01_pooling_sequences.sh &> 01_pooling_sequences.out
 time nohup bash 02_trimmomatic_q30.sh &> 02_trimmomatic_q30.out
 time nohup bash 03_cleaned_data_quality_check_by_FastQC.sh &> 03_cleaned_data_quality_check_by_FastQC.out
 time nohup bash 04_qiime2_import_PE.sh &> 04_qiime2_import_PE.out
