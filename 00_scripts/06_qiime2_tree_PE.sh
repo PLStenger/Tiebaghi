d63ed9f198e6951009e929dac88594ac0f51b9af
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2022-01-06 09:53:05 +1100

    Update 06_qiime2_tree_PE.sh

--- 00_scripts/06_qiime2_tree_PE.sh
+++ 00_scripts/06_qiime2_tree_PE.sh
@@ -62,46 +62,46 @@ qiime tools export --input-path tree/masked-aligned-RepSeq.qza --output-path exp
 ### For Bacteria
 ###############################################################
 
-cd $DATADIRECTORY_16S
-
-# Make the directory (mkdir) only if not existe already(-p)
-mkdir -p tree
-mkdir -p export/tree
-
-eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
-
-#carry out a multiple seqeunce alignment using Mafft
- qiime alignment mafft \
-  --i-sequences core/ConRepSeq.qza \
-  --o-alignment tree/aligned-RepSeq.qza
-
-##mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
-qiime alignment mask \
-  --i-alignment tree/aligned-RepSeq.qza \
-  --o-masked-alignment tree/masked-aligned-RepSeq.qza
-
-##create the tree using the Fasttree program
-qiime phylogeny fasttree \
-  --i-alignment tree/masked-aligned-RepSeq.qza \
-  --o-tree tree/unrooted-tree.qza
-
-##root the tree using the longest root
-qiime phylogeny midpoint-root \
-  --i-tree tree/unrooted-tree.qza \
-  --o-rooted-tree tree/rooted-tree.qza
-  
-  
-#export the tree  
-qiime tools export \
-  --input-path tree/unrooted-tree.qza \
-  --output-path $DATADIRECTORY_16S/tree
-  
-# This out put is in Newick format, see http://scikit-bio.org/docs/latest/generated/skbio.io.format.newick.html  
-# See it on https://itol.embl.de
-
-
-qiime tools export --input-path tree/unrooted-tree.qza --output-path export/tree/unrooted-tree
-qiime tools export --input-path tree/rooted-tree.qza --output-path export/tree/rooted-tree
-qiime tools export --input-path tree/aligned-RepSeq.qza --output-path export/tree/aligned-RepSeq
-qiime tools export --input-path tree/masked-aligned-RepSeq.qza --output-path export/tree/masked-aligned-RepSeq
+# cd $DATADIRECTORY_16S
+# 
+# # Make the directory (mkdir) only if not existe already(-p)
+# mkdir -p tree
+# mkdir -p export/tree
+# 
+# eval "$(conda shell.bash hook)"
+# conda activate qiime2-2021.4
+# 
+# #carry out a multiple seqeunce alignment using Mafft
+#  qiime alignment mafft \
+#   --i-sequences core/ConRepSeq.qza \
+#   --o-alignment tree/aligned-RepSeq.qza
+# 
+# ##mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
+# qiime alignment mask \
+#   --i-alignment tree/aligned-RepSeq.qza \
+#   --o-masked-alignment tree/masked-aligned-RepSeq.qza
+# 
+# ##create the tree using the Fasttree program
+# qiime phylogeny fasttree \
+#   --i-alignment tree/masked-aligned-RepSeq.qza \
+#   --o-tree tree/unrooted-tree.qza
+# 
+# ##root the tree using the longest root
+# qiime phylogeny midpoint-root \
+#   --i-tree tree/unrooted-tree.qza \
+#   --o-rooted-tree tree/rooted-tree.qza
+#   
+#   
+# #export the tree  
+# qiime tools export \
+#   --input-path tree/unrooted-tree.qza \
+#   --output-path $DATADIRECTORY_16S/tree
+#   
+# # This out put is in Newick format, see http://scikit-bio.org/docs/latest/generated/skbio.io.format.newick.html  
+# # See it on https://itol.embl.de
+# 
+# 
+# qiime tools export --input-path tree/unrooted-tree.qza --output-path export/tree/unrooted-tree
+# qiime tools export --input-path tree/rooted-tree.qza --output-path export/tree/rooted-tree
+# qiime tools export --input-path tree/aligned-RepSeq.qza --output-path export/tree/aligned-RepSeq
+# qiime tools export --input-path tree/masked-aligned-RepSeq.qza --output-path export/tree/masked-aligned-RepSeq
